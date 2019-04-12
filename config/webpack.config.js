a062e8ac095c4d7392dfcb12da0f7a21d5b2fffb
Jenny <jennygao@JennydeMacBook-Pro.local>
2019-04-12 17:46:32 +0800

    多页面入口，多页面出口

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,14 +1,30 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
 
 module.exports = {
-    context:path.resolve(__dirname,'../'),
+    // context:path.resolve(__dirname,'../'),
     entry: {
-      app: ['./src/index.js'],
-      about: ['./src/about.js']
+        index: ['./src/index.js'],
+        about: ['./src/about.js']
     },
     output: {
-
-      path: path.resolve(__dirname,'dist'),
-      filename: '[name].js',
-    }
-  };
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name][hash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            hash: true,
+            template: './pages/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            chunks: ['about'],
+            hash: true,
+            template: './pages/about.html'
+        }),
+    ]
+};
